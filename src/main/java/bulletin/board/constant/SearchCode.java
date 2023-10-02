3a6f0eb948774a1864d0ca95b34123ab7830e150
Arachne <jhg2819@naver.com>
2023-10-02 21:35:23 +0900

    fix : add StringPath in SearchCode ENUM

--- src/main/java/bulletin/board/constant/SearchCode.java
+++ src/main/java/bulletin/board/constant/SearchCode.java
@@ -1,8 +1,18 @@
 package bulletin.board.constant;
 
+import static bulletin.board.domain.QPost.*;
+
+import com.querydsl.core.types.dsl.StringPath;
+
 import lombok.Getter;
 
 @Getter
 public enum SearchCode {
-    TITLE, CONTENT, NAME;
+    TITLE(post.title), CONTENT(post.content), NAME(post.member.name);
+
+    private final StringPath column;
+
+    SearchCode(StringPath column) {
+        this.column = column;
+    }
 }
--- src/main/java/bulletin/board/repository/PostQueryDslRepositoryImpl.java
+++ src/main/java/bulletin/board/repository/PostQueryDslRepositoryImpl.java
@@ -3,32 +3,24 @@ package bulletin.board.repository;
 import static bulletin.board.domain.QMember.*;
 import static bulletin.board.domain.QPost.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.support.PageableExecutionUtils;
 
-import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
-import bulletin.board.constant.ErrorCode;
 import bulletin.board.constant.SearchCode;
 import bulletin.board.domain.Post;
-import bulletin.board.exceptions.PostSearchCodeException;
 import jakarta.persistence.EntityManager;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class PostQueryDslRepositoryImpl implements PostQueryDslRepository {
 
 	private final JPAQueryFactory queryFactory;
@@ -41,7 +33,7 @@ public class PostQueryDslRepositoryImpl implements PostQueryDslRepository {
 	public Page<Post> searchPosts(SearchCode searchCode, String searchString, Pageable pageable) {
 		List<Post> content = queryFactory
 			.selectFrom(post)
-			.where(searchCodeContain(searchCode, searchString))
+			.where(searchCode.getColumn().contains(searchString))
 			.leftJoin(post.member, member).fetchJoin()
 			.orderBy(getAllOrderSpecifiers(pageable).toArray(OrderSpecifier[]::new))
 			.offset(pageable.getOffset())
@@ -50,7 +42,7 @@ public class PostQueryDslRepositoryImpl implements PostQueryDslRepository {
 
 		JPAQuery<Post> countQuery = queryFactory
 			.selectFrom(post)
-			.where(searchCodeContain(searchCode, searchString));
+			.where(searchCode.getColumn().contains(searchString));
 
 		return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
 
@@ -67,25 +59,10 @@ public class PostQueryDslRepositoryImpl implements PostQueryDslRepository {
 	}
 
 	private static PathBuilder<Object> getPathBuilder(Sort.Order order) {
-		log.info("getPathBuilder post.getType={}, post.getMetadata={}, order.getProperty={}", post.getType(), post.getMetadata(), order.getProperty());
 		return new PathBuilder<>(post.getType(), post.getMetadata()).get(order.getProperty());
 	}
 
 	private static Order getDirection(Sort.Order order) {
 		return order.getDirection().isAscending() ? Order.ASC : Order.DESC;
-	}
-
-	private BooleanExpression searchCodeContain(SearchCode searchCode, String searchString) {
-		if (searchCode == SearchCode.TITLE) {
-			return post.title.contains(searchString);
-		}
-		if (searchCode == SearchCode.CONTENT) {
-			return post.content.contains(searchString);
-		}
-		if (searchCode == SearchCode.NAME) {
-			return post.member.name.contains(searchString);
-		}
-
-		throw new PostSearchCodeException(ErrorCode.INVALID_INPUT);
 	}
 }
