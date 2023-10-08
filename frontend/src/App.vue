36610b315449de3d86f599cb19950fcb5b05c2f3
Arachne <jhg2819@naver.com>
2023-10-08 22:17:05 +0900

    feat : add Login view

---
+++ frontend/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <PageHeader />
+  <router-view :key="$route.fullPath"/>
+</template>
+
+<script>
+import PageHeader from '@/components/PageHeader'
+
+export default {
+  name: 'App',
+  components: {
+    PageHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
